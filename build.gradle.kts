3a0dd354f8aa8abd14ea4b7f76dec971705a155f
71629 <105049686+71629@users.noreply.github.com>
2026-08-24 17:35:45 +0800

    Add version catalog and dependencies for Discord4J and Kotlinx libraries
    i

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.4.10"
+    kotlin("plugin.serialization") version "2.4.10"
 }
 
 group = "com.hashtag071629"
@@ -10,6 +11,10 @@ repositories {
 }
 
 dependencies {
+    implementation(libs.discord4j.core)
+    implementation(libs.kotlinx.coroutines.reactor)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.reactor.kotlin.extensions)
     testImplementation(kotlin("test"))
 }
 
