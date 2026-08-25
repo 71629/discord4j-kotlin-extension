d826928aa3f4db00aff96407fd07beeeda9e7f0b
71629 <105049686+71629@users.noreply.github.com>
2026-08-25 20:24:56 +0800

    Add `maven-publish` plugin and configure local publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.4.10"
     kotlin("plugin.serialization") version "2.4.10"
+
+    `maven-publish`
 }
 
 group = "com.hashtag071629"
@@ -8,6 +10,20 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+    }
+
+    publications {
+        create<MavenPublication>("library") {
+            groupId = group as String
+            artifactId = "discord4j-kotlin-extension"
+            from(components["java"])
+        }
+    }
 }
 
 dependencies {
