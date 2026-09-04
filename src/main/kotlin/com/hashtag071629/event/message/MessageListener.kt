fd39ad9921eeb724ebb6332a9473c53e19e00771
71629 <105049686+71629@users.noreply.github.com>
2026-09-04 15:29:22 +0800

    Refactor `MessageListener`: extract shared logic into `EventListener` base class to improve reusability and simplify event handling.

--- src/main/kotlin/com/hashtag071629/event/message/MessageListener.kt
+++ src/main/kotlin/com/hashtag071629/event/message/MessageListener.kt
@@ -1,49 +1,52 @@
 package com.hashtag071629.event.message
 
 import com.hashtag071629.annotations.ClientMarker
-import com.hashtag071629.event.GatewayEventDefinition
 import discord4j.core.event.domain.Event
 import discord4j.core.event.domain.message.MessageEvent
 import kotlinx.coroutines.reactor.mono
 import reactor.core.publisher.Flux
 
 @ClientMarker
-public abstract class MessageListener<T : MessageEvent> internal constructor() {
-    private val listeners = mutableListOf<Definition<T>>()
-    public var maxConcurrency: Int = Int.MAX_VALUE
-
-    private val definition get() = Definition<T>()
+public abstract class EventListener<T : Event> internal constructor() {
+    protected val listeners: MutableList<Definition<T>> = mutableListOf()
+    protected val definition: Definition<T> get() = Definition()
 
     public fun install(config: Definition<T>.() -> Unit) {
-        val listener = definition.apply(config)
-        listeners.add(listener)
+        val def = definition.apply(config)
+        listeners.add(def)
     }
 
-    internal fun handle(event: T) {
+    protected abstract suspend fun handle(event: T)
+
+    protected suspend fun Definition<T>.handle(event: T) {
+        runCatching {
+            action.invoke(event)
+        }.onFailure {
+            it.printStackTrace()
+            onException?.invoke(event, it)
+        }
+    }
+
+    public fun Definition<T>.onException(block: suspend (T, Throwable) -> Unit) {
+        onException = block
+    }
+
+    public open class Definition<T : Event> internal constructor() {
+        internal open var predicate: (T) -> Boolean = { true }
+        internal var action: suspend (T) -> Unit = {}
+        internal var onException: (suspend (T, Throwable) -> Unit)? = null
+    }
+}
+
+public abstract class MessageListener<T : MessageEvent> internal constructor() : EventListener<T>() {
+    public var maxConcurrency: Int = Int.MAX_VALUE
+
+    override suspend fun handle(event: T) {
         val matches = listeners.filter { it.predicate(event) }.map { suspend { it.handle(event) } }
         Flux.fromIterable(matches).flatMap({ mono { it() } }, maxConcurrency).subscribe()
     }
 
-    public class Definition<T : Event> internal constructor() : GatewayEventDefinition<T>() {
-        internal var predicate: (T) -> Boolean = { true }
-        internal var action: suspend (T) -> Unit = {}
-        internal var onException: (suspend (T, Throwable) -> Unit)? = null
-
-        public fun condition(block: (T) -> Boolean) {
-            predicate = block
-        }
-
-        override suspend fun handle(event: T) {
-            runCatching {
-                action.invoke(event)
-            }.onFailure {
-                super.onException(event, it)
-                onException?.invoke(event, it)
-            }
-        }
-
-        public fun onException(block: suspend (T, Throwable) -> Unit) {
-            onException = block
-        }
+    public fun Definition<T>.condition(block: (T) -> Boolean) {
+        predicate = block
     }
 }
