d6c25b9a0717ae2658b0715cd1a421bfdbdb9f4d
71629 <105049686+71629@users.noreply.github.com>
2026-09-07 19:53:23 +0800

    Refactor `CentralButtonEventHandler` to simplify event handling and remove redundant `ButtonActionDefinition` and `ButtonConfigurator` classes

--- src/main/kotlin/com/hashtag071629/component/CentralButtonEventHandler.kt
+++ src/main/kotlin/com/hashtag071629/component/CentralButtonEventHandler.kt
@@ -1,30 +1,27 @@
 package com.hashtag071629.component
 
-import com.hashtag071629.client
+import com.hashtag071629.event.EventListener
+import com.hashtag071629.event.slash.SlashCommand
+import com.hashtag071629.event.slash.SlashCommand.handle
 import discord4j.core.GatewayDiscordClient
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent
 import kotlinx.coroutines.reactor.mono
 
-public object CentralButtonEventHandler {
-    internal val listeners = mutableMapOf<String, ButtonActionDefinition>()
+public object CentralButtonEventHandler : EventListener<ButtonInteractionEvent, CentralButtonEventHandler.Definition>() {
+    override val definition: Definition get() = Definition()
 
-    init {
-        client.on(ButtonInteractionEvent::class.java) { mono { handle(it) } }
+    public fun GatewayDiscordClient.button(customId: String, config: Definition.() -> Unit) {
+        definition.apply(config).also { it.customId = customId }
+
+        on(ButtonInteractionEvent::class.java) { mono { handle(it) } }.subscribe()
     }
 
-    public fun GatewayDiscordClient.button(config: ButtonConfigurator.() -> Unit) {
-        ButtonConfigurator().apply(config)
-
-        on(ButtonInteractionEvent::class.java) { mono { handle(it) } }.subscribe {  }
+    override suspend fun handle(event: ButtonInteractionEvent) {
+        listeners.firstOrNull { it.predicate(event) }?.handle(event)
     }
 
-    private suspend fun handle(event: ButtonInteractionEvent) {
-        listeners[event.customId]?.let { handler ->
-            runCatching {
-                handler.onClick(event)
-            }.onFailure {
-                handler.onException(event, it)
-            }
-        }
+    public class Definition internal constructor() : EventListener.Definition<ButtonInteractionEvent>() {
+        internal lateinit var customId: String
+        override var predicate: (ButtonInteractionEvent) -> Boolean = { it.customId == customId }
     }
 }
